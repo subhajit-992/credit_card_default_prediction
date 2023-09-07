ba5604ccf8865a75aef79e8b24a0e354754fc105
Subhajit <subhajit992@gmail.com>
2023-09-07 19:28:16 +0530

    project commit

--- creditcard/component/data_validation.py
+++ creditcard/component/data_validation.py
@@ -15,7 +15,7 @@ class DataValidation:
     def __init__(self,data_validation_config:DataValidationConfig, 
                  data_ingestion_artifact:DataIngestionArtifact):
         try:
-            logging.info(f"{'>>'*20} Data validation {'<<'*20}")
+            logging.info(f"{'>>'*20} Data Validation {'<<'*20}")
             self.data_validation_config = data_validation_config
             self.data_ingestion_artifact = data_ingestion_artifact
             self.validation_error=dict()
